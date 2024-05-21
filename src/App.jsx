08c41d3818f236590903510de7bbcc9091686f2e
AaradhyaParsodia <aaradhyaparsodia@gmail.com>
2024-05-22 03:19:02 +0530

    Temp UI table implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import ColumnHead from "../components/ColumnHead";
+import RowContent from "../components/RowContent";
+import "./App.css";
+
+const tableHead = ["rating", "author name", "title", "first Publish Year", "subject", "dob", "topWork"];
+
+const tableContent = [{
+    "rating": "4.5/5",
+    "authorName": "J. K. Rowling",
+    "title": "Official Site",
+    "firstPublishYear": "1997",
+    "subject": "Fantasy",
+    "dob": "31 July 1965",
+    "topWork": "Harry Potter and the Philosopher's Stone"
+}]
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div className=' bg-black'>
-        something
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <ColumnHead tableHead={tableHead}></ColumnHead>
+                    <RowContent tableContent={tableContent}></RowContent>
+                </table>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
